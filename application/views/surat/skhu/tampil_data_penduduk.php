7ea31b15660ba492d868a319f408956b8400b4f3
tralala48 <gian48megantara@gmail.com>
2017-10-15 20:40:55 +0700

    Update tampil_data_penduduk.php
    mengganti baris kode untuk menampilkan data,

--- application/views/surat/skhu/tampil_data_penduduk.php
+++ application/views/surat/skhu/tampil_data_penduduk.php
@@ -1,63 +1,64 @@
 <table class="profil">
+	<?php 
+	if (is_array($penduduk)||is_object($penduduk)) {
+		foreach ($penduduk as $row){
+		?>
 	<tr>
 		<td>NIK</td>
-		<td>:</td>
-		<td><?= $penduduk->NIK; ?></td>
-	</tr>
-	<tr>
+		<td><?php echo $row['nik']; ?></td>
+	</tr>	
+	<tr>	
 		<td>Nama</td>
-		<td>:</td>
-		<td><?= $penduduk->NAMA; ?></td>
+		<td><?php echo $row['nama']; ?></td>
+	</tr>
+	<tr>	
+		<td>Jenis Kelamin</td> 
+		<td><?php echo $row['jeniskelamin']; ?></td>
+	</tr>
+	<tr>	
+		<td>Tempat Lahir</td> 
+		<td><?php echo $row['tempatlahir']; ?></td>
+	</tr>
+	<tr>	
+		<td>Tanggal Lahir</td> 
+		<td><?php echo $row['tanggallahir']; ?></td>
+	</tr>
+	<tr>	
+		<td>Umur</td> 
+		<td><?php echo $row['umur']; ?></td>
+	</tr>
+	<tr>	
+		<td>Agama</td> 
+		<td><?php echo $row['agama']; ?></td>
+	</tr>
+	<tr>	
+		<td>Status</td> 
+		<td><?php echo $row['status']; ?></td>
+	</tr>
+	<tr>	
+		<td>Pendidikan</td> 
+		<td><?php echo $row['pendidikan']; ?></td>
+	</tr>
+	<tr>	
+		<td>Pekerjaan</td> 
+		<td><?php echo $row['pekerjaan']; ?></td>
+	</tr>
+	<tr>	
+		<td>Nama Ibu</td> 
+		<td><?php echo $row['namaibu']; ?></td>
+	</tr>
+	<tr>	
+		<td>Nama Ayah</td> 
+		<td><?php echo $row['namaayah']; ?></td>
 	</tr>
 	<tr>
-		<td>Jenis Kelamin</td>
-		<td>:</td>
-		<td><?= $penduduk->JENISKELAMIN; ?></td>
+		<td colspan="2" align="center">
+			<br>
+			<a href="<?= base_url('surat/skhu/download/'.$row['nik']); ?>" class="btn btn-primary" role="button">Download Surat</a>
+		</td>
 	</tr>
-	<tr>
-		<td>Tempat Lahir</td>
-		<td>:</td>
-		<td><?= $penduduk->TEMPATLAHIR; ?></td>
-	</tr>
-	<tr>
-		<td>Tanggal Lahir</td>
-		<td>:</td>
-		<td><?= $penduduk->TANGGALLAHIR; ?></td>
-	</tr>
-	<tr>
-		<td>Umur</td>
-		<td>:</td>
-		<td><?= $penduduk->UMUR; ?></td>
-	</tr>
-	<tr>
-		<td>Agama</td>
-		<td>:</td>
-		<td><?= $penduduk->AGAMA; ?></td>
-	</tr>
-	<tr>
-		<td>Status</td>
-		<td>:</td>
-		<td><?= $penduduk->STATUS; ?></td>
-	</tr>
-	<tr>
-		<td>Pendidikan</td>
-		<td>:</td>
-		<td><?= $penduduk->PENDIDIKAN; ?></td>
-	</tr>
-	<tr>
-		<td>Pekerjaan</td>
-		<td>:</td>
-		<td><?= $penduduk->PEKERJAAN; ?></td>
-	</tr>
-	<tr>
-		<td>Nama Ibu</td>
-		<td>:</td>
-		<td><?= $penduduk->NAMAIBU; ?></td>
-	</tr>
-	<tr>
-		<td>Nama Ayah</td>
-		<td>:</td>
-		<td><?= $penduduk->NAMAAYAH; ?></td>
-	</tr>
+	<?php
+		}
+	}
+	?>
 </table>
-<a href="<?= base_url('surat/skhu/download/'.$penduduk->NIK); ?>" class="btn btn-primary" role="button">Download Surat</a>
